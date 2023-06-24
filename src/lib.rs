2906ce218244ef764b4bd3fe2e3646862cd9e56f
Bobbbay <abatterysingle@gmail.com>
2023-06-24 17:10:06 +0000

    chore: structural changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
+/// Marker trait for all kinds of Tarsier Data. Data is a piece of state
+/// that is Copy, small, and efficient.
+pub trait Data: Copy {}
